f399f2609cee9a9bc807a25369ec50382ffee897
SWC <st1020575@gmail.com>
2022-12-18 05:16:08 +0800

    Fix details.

--- Assets/Scripts/Trap/fallHole.cs
+++ Assets/Scripts/Trap/fallHole.cs
@@ -7,18 +7,21 @@ public class fallHole : MonoBehaviour {
     [SerializeField] private GameObject player;
     [SerializeField] private Animator zombieAnimator;
     [SerializeField] private GameObject retryCanvas;
+    [SerializeField] private Light roomLight;
+    private bool isEnter = false;
     IEnumerator fallHoleTeleport() {
         this.zombieAnimator.SetBool("isScare", true);
         this.player.GetComponent<FPController>().enabled = false;
         this.player.GetComponent<LidarProject.Scanner>().enabled = false;
+        yield return new WaitForSeconds(0.6f);
         for (int i = 0; i < this.player.transform.GetChild(0).childCount; i++)
             this.player.transform.GetChild(0).GetChild(i).gameObject.SetActive(false);
-
-        yield return new WaitForSeconds(0.35f);
         this.player.transform.GetChild(0).rotation = new Quaternion(0, 0, 0, 0);
         this.player.transform.position = new Vector3(100f, 0.7f, 100.3f);
         this.player.transform.rotation = Quaternion.Euler(Vector3.zero);
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(0.4f);
+        this.isEnter = true;
+        yield return new WaitForSeconds(0.8f);
         this.retryCanvas.SetActive(true);
         Time.timeScale = 0;
     }
@@ -26,9 +29,14 @@ public class fallHole : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.M))
             StartCoroutine(this.fallHoleTeleport());
     }
+    void FixedUpdate() {
+        if (this.isEnter && roomLight.intensity < 0.6)
+            roomLight.intensity += 0.02f;
+    }
     void OnTriggerEnter(Collider other) {
         if (other.tag == "Player")
             StartCoroutine(this.fallHoleTeleport());
+
     }
     public void reloadGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
