22822fdca876eca23e6ec912f8c699dc6ebe07e6
poyu <yoyohihirobert@gmail.com>
2022-12-20 00:51:36 +0800

    .

--- Assets/Scripts/UI/passwordDoor.cs
+++ Assets/Scripts/UI/passwordDoor.cs
@@ -66,7 +66,6 @@ public class passwordDoor : MonoBehaviour
             for (int i = 0; i < 10; i++)
                 numberWithGrayFilter[i].SetActive(false);
             this.password = "";
-            return;
         }
         this.password = "";
         Cursor.visible = false;
