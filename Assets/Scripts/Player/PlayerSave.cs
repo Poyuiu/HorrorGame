d5711877f3c20d661cef0c730e52022b25d2e9da
Poyu Juan <yoyohihirobert@gmail.com>
2023-01-12 16:15:48 +0800

    [FIX] fixed some small bugs

--- Assets/Scripts/Player/PlayerSave.cs
+++ Assets/Scripts/Player/PlayerSave.cs
@@ -2,33 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSave : MonoBehaviour {
-	public int nowSP { get; set; }
-	private const string fileName = "player.json";
-	private string filePath;
-	Save.PlayerData playerData;
-	void Awake() {
-		// uncomment below if you want to clear save data
-		PlayerPrefs.DeleteAll();
-		nowSP = 0;
-		Load();
-        switch(nowSP){
+public class PlayerSave : MonoBehaviour
+{
+    public int nowSP { get; set; }
+    private const string fileName = "player.json";
+    private string filePath;
+    Save.PlayerData playerData;
+    void Awake()
+    {
+        //uncomment below if you want to clear save data
+        PlayerPrefs.DeleteAll();
+        nowSP = 0;
+        Load();
+        switch (nowSP)
+        {
             case 1:
-                this.transform.position = new Vector3(22f,1f,-2.5f);
-                this.transform.rotation = Quaternion.Euler(new Vector3(0f,270f,0f));
+                this.transform.position = new Vector3(22f, 1f, -2.5f);
+                this.transform.rotation = Quaternion.Euler(new Vector3(0f, 270f, 0f));
                 break;
         }
-	}
-	
-	public void Save(int saveSp) {
-		playerData = new Save.PlayerData();
-		playerData.sp = saveSp;
+    }
+
+    public void Save(int saveSp)
+    {
+        playerData = new Save.PlayerData();
+        playerData.sp = saveSp;
         nowSP = saveSp;
-		PlayerPrefs.SetString("PlayerData", JsonUtility.ToJson(playerData));
-	}
-	private void Load() {
-		playerData = JsonUtility.FromJson<Save.PlayerData>(PlayerPrefs.GetString("PlayerData"));
+        PlayerPrefs.SetString("PlayerData", JsonUtility.ToJson(playerData));
+    }
+    private void Load()
+    {
+        playerData = JsonUtility.FromJson<Save.PlayerData>(PlayerPrefs.GetString("PlayerData"));
         if (playerData != null)
-			nowSP = playerData.sp;
-	}
+            nowSP = playerData.sp;
+    }
 }
