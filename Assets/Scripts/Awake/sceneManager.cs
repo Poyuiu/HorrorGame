800f2356833b06a079367e89a108e0a1ceb146ff
アオカ <yongyin0101@gmail.com>
2023-01-13 13:10:20 +0800

    add Awake Scene

---
+++ Assets/Scripts/Awake/sceneManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    public GameObject focusOn;
+    public float Speed = 1;
+    public float desiredTime1 = 5.0f;
+    public float desiredTime2 = 10.0f;
+    private float timer = 0;
+
+    void Start() {
+
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= desiredTime1)
+        {
+            focusOn.transform.localPosition = Vector3.MoveTowards(focusOn.transform.localPosition, new Vector3(-14.974f, 0.879f, 4.754f), Speed*Time.deltaTime);
+        }
+        if (timer >= desiredTime2)
+        {
+            SceneManager.LoadScene("stage1");
+        }
+    }
+
+}
